2b41cdcb22b6137ac763c6c87117f243a7dad45f
Erling Rennemo Jellum <erlingrj@stud.ntnu.no>
2020-03-24 10:29:08 +0100

    update bake

--- bake.sh
+++ bake.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 BOOM_SRCS="/home/erling/chipyard/generators/boom/src/main/scala"
-REMOTE_SRCS="chipyard/generators/boom/src/main/scala/"
-eval "ssh $NUBUNTU ls -R --full-time $REMOTE_SRCS > .tmp"
+eval "ssh $VUBUNTU ls -R --full-time $BOOM_SRCS > .tmp"
 cdir=""
 while IFS="" read -r p || [ -n "$p" ]
 do
-	if [[ ${p::1} == "c" ]]; then cdir=$p;
+	if [[ ${p::1} == "/" ]]; then cdir=${p::-1};
 	elif [[ ${p::1} == "-" ]]; 
 	then
-		name="/home/erling/$cdir/$(echo $p | awk '{print $9}')"
+		name="$cdir/$(echo $p | awk '{print $9}')"
 		rtimestamp=$(date -d "$(echo $p | awk {'printf ("%s %s\n", $6,$7)'})" +"%s")
-		echo $name $rtimestamp
 
 		# Now compare it to the local version of that file
-		if [ ! -f $name]; then
-			echo "scp $VUBUNTU:$name $name"
+		if [ ! -f $name ]; then
+      echo "$name does not exist locally. Copy"
+			eval "scp $VUBUNTU:$name $name"
 		else
 			# File exist. Compare timestamps
-			ltimestamp=$(date -d "$(ls --full-time $name | awk {'printf("%s %s\n", $6, $7'})" +"%s")
+			ltimestamp=$(date -d "$(ls --full-time $name | awk {'printf("%s %s\n",
+      $6, $7)'})" +"%s")
 			if [ $rtimestamp -ge $ltimestamp ];
 			then
-				echo "scp $VUBUNTU:$name $name"
+				eval "scp $VUBUNTU:$name $name"
 			fi
 		fi
 	fi	
 done < .tmp
-echo "rm .tmp"
+eval "rm .tmp"
 
